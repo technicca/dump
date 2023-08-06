d044b25a36b3b5e759b8db710df1cddd479a2288
technicca <cake3327@gmail.com>
2023-08-06 16:12:38 +0300

    rps, textual

--- python/rps.py
+++ python/rps.py
@@ -1,25 +1,113 @@
-def execute_option(option):
-    if option == '1':
-        print("You chose Option 1")
-        # Add your code for Option 1 here
-    elif option == '2':
-        print("You chose Option 2")
-        # Add your code for Option 2 here
-    elif option == '3':
-        print("You chose Option 3")
-        # Add your code for Option 3 here
-    else:
-        print("Invalid option")
+import curses
+import random
 
-while True:
-    print("\nChoose one of the following options:")
-    print("1. Option 1")
-    print("2. Option 2")
-    print("3. Option 3")
-    print("4. Exit")
+ASCII_ART = """
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠶⣆⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣸⣏⣀⣀⣽⣄⣀⡉⣿⠿⢿⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⠛⠉⠀⠀⠀⠀⢈⡟⢿⠀⠀⢸⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣾⣿⣿⣿⡀⠀⢠⡤⠤⢴⠏⠀⢸⠀⠀⢸⠀⠈⠙⢿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣠⣄⠀⢀⣾⣿⣿⣿⣿⣿⡇⠀⠀⣷⡤⠾⣤⣀⣼⣄⡀⢸⠀⠀⡆⠘⣿⣿⣿⣿⣦⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀
+⠀⣠⠶⣾⠁⠈⠓⢿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠘⠋⠙⠓⢦⣄⠈⠉⠉⠓⠾⠁⢰⣿⣿⣿⣿⣿⣧⠀⢀⡼⠁⠉⡦⠀⠀⠀⠀
+⠰⣇⠀⠀⠙⢦⡀⠀⠙⢿⣿⣿⣿⣿⣷⣄⠀⠀⠀⠀⠀⠀⠈⠓⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣿⣿⣷⠋⠀⢀⠞⠁⠀⠀⠀⠀
+⢀⡜⠳⣄⠀⠀⠙⢦⡀⠀⠙⢿⣿⣿⠿⠿⣧⣀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿⣿⣿⠟⠁⢀⡴⠋⠀⠀⠀⠀⠀⠀
+⠸⣄⠀⠈⠳⣄⠀⠀⠙⠦⠀⠀⠈⠻⣄⠀⠀⠈⠹⣄⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⠟⠁⠀⣠⡞⠀⠀⣀⣀⡤⠤⠔⡄
+⠀⣸⠷⣄⠀⠈⠳⠄⠀⠀⠀⠀⠀⠀⠈⠓⠀⠀⠀⠘⣦⠀⠀⠀⣠⠏⠁⠉⠛⠿⣟⠉⠀⠀⠀⠸⠿⠓⠊⠉⠁⠀⠀⠀⢀⡷
+⠀⠻⣆⡈⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⢀⡼⠁⠀⠀⠀⢸⣄⡀⠙⢲⠀⠀⠀⣀⣀⡠⠤⠤⠒⠒⠋⠁⠀
+⠀⠀⠈⠙⢦⡀⠙⠓⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡼⠀⢸⠀⠀⠀⠀⠀⡸⠈⡇⠀⣸⠋⠉⠉⢻⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⡇⠀⠈⣇⠀⠀⠀⠰⠁⡸⠛⠲⠷⠖⠒⢶⠾⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠻⣦⣀⠀⠀⠀⠀⠀⠀⠀⢀⡤⠀⠙⢦⡴⠋⠀⠀⠀⠀⠀⠉⠒⠒⠦⠤⢀⡼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣦⣄⣀⣠⡤⠖⠋⠀⠀⠀⠀⠙⠦⠀⠀⠀⢀⣀⣀⣀⣀⣤⡶⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣧⣀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠛⠛⠛⠋⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+"""
 
-    user_choice = input("\nEnter your choice: ")
-    if user_choice == '4':
-        break
 
-    execute_option(user_choice)
+def crop_art(art, max_y, max_x):
+    art_lines = art.split('\n')
+    cropped_art_lines = [line[:max_x] for line in art_lines[:max_y]]
+    return '\n'.join(cropped_art_lines)
+
+
+def main(stdscr):
+
+    # turn off cursor blinking
+    curses.curs_set(0)
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+    current_row = 0
+
+    options = ['🪨 Rock', '✋Paper', '✂️ Scissors', 'Exit']
+
+    # Read scores from file
+    try:
+        with open('scores.txt', 'r') as f:
+            player_score, ai_score = map(int, f.read().split())
+    except FileNotFoundError:
+        player_score = ai_score = 0
+    
+    ai_choice = ''
+    user_choice = ''
+    result = ''
+
+    while True:
+        
+        h, w = stdscr.getmaxyx()
+        cropped_art = crop_art(ASCII_ART, h, w)
+        stdscr.addstr(0, 0, cropped_art)
+
+        for idx, row in enumerate(options):
+            x = w//2 - len(row)//2
+            y = h//2 - len(options)//2 + idx
+
+            if idx == current_row:
+                stdscr.attron(curses.color_pair(1))
+                stdscr.addstr(y, x, row)
+                stdscr.attroff(curses.color_pair(1))
+            else:
+                stdscr.addstr(y, x, row)
+
+        score_str = f"Score: Player {player_score} - AI {ai_score}"
+        score_x = w - len(score_str) - 1  # -1 to leave a space at the end
+        stdscr.addstr(h-1, score_x, score_str)
+
+        key = stdscr.getch()
+
+        if key == curses.KEY_UP and current_row > 0:
+            current_row -= 1
+        elif key == curses.KEY_DOWN and current_row < len(options)-1:
+            current_row += 1
+        elif key == curses.KEY_ENTER or key in [10, 13]:
+            ai_choice = random.choice(['rock', 'paper','scissors']) # AI chooses randomly
+            if options[current_row] == "Exit":
+                # Write scores to file
+                with open('scores.txt', 'w') as f:
+                    f.write(f'{player_score} {ai_score}')
+                break
+            elif options[current_row] == "🪨 Rock":
+                user_choice = 'rock'
+            elif options[current_row] == "✋Paper":
+                user_choice = 'paper'
+            else:
+                if options[current_row] == "✂️ Scissors":
+                    user_choice ='scissors'
+            if user_choice == ai_choice:
+                result = 'Tie'
+            elif (user_choice == 'rock' and ai_choice == 'scissors') or (user_choice == 'paper' and ai_choice == 'rock') or (user_choice =='scissors' and ai_choice == 'paper'):
+                result = 'Win'
+                player_score += 1
+            else:
+                result = 'Lose'
+                ai_score += 1
+
+            result_str = "You selected '{}'. AI selected '{}'. You {}!".format(user_choice, ai_choice, result)
+            result_x = w - len(result_str) - 1  # -1 to leave a space at the end
+            stdscr.addstr(h-2, result_x, result_str)
+
+            stdscr.refresh()
+            stdscr.getch()
+
+        stdscr.refresh()
+
+
+
+curses.wrapper(main)
